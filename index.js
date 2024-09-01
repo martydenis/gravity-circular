864d686dd284cf2a1fc1c59719731cbd98e594bc
martydenis <martin.2nis@gmail.com>
2024-09-01 11:08:23 +0200

    Using git submodule

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+import { dpi, hexToRGB, randomIntFromRange, randomValueFromArray, getDistance, initControls, initStopPropagation } from './sandbox-utils/utilities.js';
+
+initControls();
+initStopPropagation();
+
 // Variables
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 const FPS = 60;
-const INTERVAL = Math.round(1000 / FPS) // ms
 
 var gravity = 0.3;
 var circles = [];
@@ -11,8 +15,8 @@ var circles = [];
 //   y: innerHeight / 2,
 // };
 var center = {
-  x: innerWidth / 2,
-  y: innerHeight / 2,
+    x: innerWidth / 2,
+    y: innerHeight / 2,
 }
 // var oldCenter = {
 //   x: innerWidth / 2,
@@ -21,143 +25,150 @@ var center = {
 var circleRadius = 80;
 var bgColor = hexToRGB('#475C7A', 0.4);
 var colors = [
-  '#FCBB6D',
-  '#D8737F',
-  '#AB6C8C',
-  '#685D79'
+    '#FCBB6D',
+    '#D8737F',
+    '#AB6C8C',
+    '#685D79'
 ];
 
 // Objects
 class Circle {
-  constructor(x, y, radius, angle, mass, color) {
-    this.x = x;
-    this.y = y;
-    this.distance = circleRadius;
-    this.radius = radius;
-    this.angle = angle;
-    this.mass = mass;
-    this.color = color;
-    this.velocity = {
-      h: 0,
-      v: 0
-    };
-    this.wallFriction = 0.80;
-    this.airFriction = 0.995;
-  }
-
-  draw() {
-    ctx.beginPath();
-    ctx.arc(dpi(this.x), dpi(this.y), dpi(this.radius), 0, 2 * Math.PI);
-    ctx.fillStyle = this.color;
-    ctx.fill();
-    ctx.closePath();
-  }
-
-  update() {
-    this.distance = getDistance(this.x, this.y, center.x, center.y);
-
-    if (this.distance - this.radius + this.velocity.v + gravity < circleRadius) {
-      this.velocity.h *= this.wallFriction;
-      this.velocity.v = -this.velocity.v * this.wallFriction;
-
-      this.distance = circleRadius + this.radius;
-    } else {
-      this.velocity.v -= gravity;
+    constructor(x, y, radius, angle, mass, color) {
+        this.x = x;
+        this.y = y;
+        this.distance = circleRadius;
+        this.radius = radius;
+        this.angle = angle;
+        this.mass = mass;
+        this.color = color;
+        this.velocity = {
+            h: 0,
+            v: 0
+        };
+        this.wallFriction = 0.80;
+        this.airFriction = 0.995;
     }
 
-    this.velocity.h *= this.airFriction;
-    this.velocity.v *= this.airFriction;
-
-    if (this.angle + this.velocity.h < 0) {
-      this.angle = 2 * Math.PI;
-    } else if (this.angle + this.velocity.h > 2 * Math.PI) {
-      this.angle = 0;
-    } else {
-      this.angle += this.velocity.h;
+    draw() {
+        ctx.beginPath();
+        ctx.arc(dpi(this.x), dpi(this.y), dpi(this.radius), 0, 2 * Math.PI);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+        ctx.closePath();
     }
 
-    this.distance += this.velocity.v;
+    update(delta) {
+        this.distance = getDistance(this.x, this.y, center.x, center.y);
 
-    this.x = center.x + (Math.cos(this.angle) * (this.distance));
-    this.y = center.y + (Math.sin(this.angle) * (this.distance));
-  }
+        if (this.distance - this.radius + this.velocity.v + gravity < circleRadius) {
+            this.velocity.h *= this.wallFriction;
+            this.velocity.v = -this.velocity.v * this.wallFriction;
 
-  jump(force) {
-    const hForce = randomIntFromRange(-force, force) / 40;
-    const vForce = force * 60;
-    this.velocity.h = hForce / this.mass;
-    this.velocity.v = vForce / this.mass;
-  }
+            this.distance = circleRadius + this.radius;
+        } else {
+            this.velocity.v -= gravity;
+        }
+
+        this.velocity.h *= this.airFriction;
+        this.velocity.v *= this.airFriction;
+
+        if (this.angle + this.velocity.h < 0) {
+            this.angle = 2 * Math.PI;
+        } else if (this.angle + this.velocity.h > 2 * Math.PI) {
+            this.angle = 0;
+        } else {
+            this.angle += this.velocity.h;
+        }
+
+        this.distance += this.velocity.v * delta;
+
+        this.x = center.x + (Math.cos(this.angle) * (this.distance));
+        this.y = center.y + (Math.sin(this.angle) * (this.distance));
+    }
+
+    jump(force) {
+        const hForce = randomIntFromRange(-force, force) / 40;
+        const vForce = force * 60;
+        this.velocity.h = hForce / this.mass;
+        this.velocity.v = vForce / this.mass;
+    }
 }
 
 
 // Functions
 function initScene() {
-  circles = [];
-  center = {
-    x: innerWidth / 2,
-    y: innerHeight / 2,
-  }
+    circles = [];
+    center = {
+        x: innerWidth / 2,
+        y: innerHeight / 2,
+    }
 
-  for (let i = 0; i < 70; i++) {
-    const radius = randomIntFromRange(6, 12);
-    const mass = 30 + radius;
-    const color = randomValueFromArray(colors);
-    const angle = randomIntFromRange(0, 2 * Math.PI, false);
-    let x = innerWidth / 2;
-    let y = innerHeight / 2;
-    x += Math.cos(angle) * (circleRadius + radius);
-    y += Math.sin(angle) * (circleRadius + radius);
+    for (let i = 0; i < 70; i++) {
+        const radius = randomIntFromRange(6, 12);
+        const mass = 30 + radius;
+        const color = randomValueFromArray(colors);
+        const angle = randomIntFromRange(0, 2 * Math.PI, false);
+        let x = innerWidth / 2;
+        let y = innerHeight / 2;
+        x += Math.cos(angle) * (circleRadius + radius);
+        y += Math.sin(angle) * (circleRadius + radius);
 
-    circles.push(new Circle(x, y, radius, angle, mass, color));
-  }
+        circles.push(new Circle(x, y, radius, angle, mass, color));
+    }
 }
 
-function updateGameLogic() {
-  for (let i = 0; i < circles.length; i++) {
-    const circle = circles[i];
-    circle.update();
-  }
+let oldTime = new Date();
+let newTime = new Date();
+function updateGame() {
+    newTime = new Date();
+    const delta = (newTime - oldTime) * 0.075;
+
+    updateLogic(delta);
+    updateVisuals();
+
+    oldTime = new Date();
+    requestAnimationFrame(updateGame);
 }
 
-function updateGameDisplay() {
-  ctx.beginPath()
-  ctx.rect(0, 0, dpi(innerWidth), dpi(innerHeight));
-  ctx.fillStyle = bgColor;
-  ctx.fill();
+function updateLogic(delta) {
+    for (const circle of circles) {
+        circle.update(delta);
+    }
+}
 
-  for (let i = 0; i < circles.length; i++) {
-    const circle = circles[i];
-    circle.draw();
-  }
+function updateVisuals() {
+    ctx.beginPath()
+    ctx.rect(0, 0, dpi(innerWidth), dpi(innerHeight));
+    ctx.fillStyle = bgColor;
+    ctx.fill();
 
-  requestAnimationFrame(updateGameDisplay);
+    for (const circle of circles) {
+        circle.draw();
+    }
 }
 
 function resizeWindow() {
-  canvas.width = dpi(window.innerWidth);
-  canvas.height = dpi(window.innerHeight);
+    canvas.width = dpi(window.innerWidth);
+    canvas.height = dpi(window.innerHeight);
 }
 
 
 // Initializations
 initScene();
-let gameLogicInterval = new AdjustingInterval(updateGameLogic, INTERVAL);
-gameLogicInterval.start();
-updateGameDisplay();
+updateGame();
 resizeWindow();
 
 
 // Event Listeners
 window.addEventListener('resize', function () {
-  resizeWindow();
-  initScene();
+    resizeWindow();
+    initScene();
 });
 
 window.addEventListener('click', function () {
-  for (let i = 0; i < circles.length; i++) {
-    const circle = circles[i];
+    for (let i = 0; i < circles.length; i++) {
+        const circle = circles[i];
 
-    circle.jump(8);
-  }
+        circle.jump(8);
+    }
 });
--- index.php
+++ index.php
@@ -1,43 +1,52 @@
-<?php require_once '../../_includes.php'; ?>
 <!DOCTYPE html>
-<html lang="<?php echo $language_iso; ?>">
-  <head>
-    <?php include_once '../../_games_head.php'; ?>
-    <title>Circular Gravity</title>
-  </head>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Bungee&display=swap" rel="stylesheet">
+        <link rel="icon" type="image/svg" href="../martindenis/favicon.svg" />
+        <link rel="stylesheet" type="text/css" href="./sandbox-utils/style.css" media="screen" />
+        <link rel="stylesheet" type="text/css" href="./index.css" media="screen" />
+        <title>Circular Gravity</title>
+    </head>
+    <body>
+        <canvas id="canvas" width="400" height="400"></canvas>
 
-  <body>
-    <canvas id="canvas" width="400" height="400"></canvas>
+        <button class="controls__toggler-button controls--open js-controls-toggler js-stop-propagation">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-help-circle">
+                <circle cx="12" cy="12" r="10"></circle>
+                <path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3"></path>
+                <line x1="12" y1="17" x2="12.01" y2="17"></line>
+            </svg>
+        </button>
+        <button class="controls__toggler-button controls--closed js-controls-toggler js-stop-propagation">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x">
+                <line x1="18" y1="6" x2="6" y2="18"></line>
+                <line x1="6" y1="6" x2="18" y2="18"></line>
+            </svg>
+            <!-- <span>Close</span> -->
+        </button>
 
-    <button class="controls__toggler-button controls--open js-controls-toggler js-stop-propagation">
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-sliders"><line x1="4" y1="21" x2="4" y2="14"></line><line x1="4" y1="10" x2="4" y2="3"></line><line x1="12" y1="21" x2="12" y2="12"></line><line x1="12" y1="8" x2="12" y2="3"></line><line x1="20" y1="21" x2="20" y2="16"></line><line x1="20" y1="12" x2="20" y2="3"></line><line x1="1" y1="14" x2="7" y2="14"></line><line x1="9" y1="8" x2="15" y2="8"></line><line x1="17" y1="16" x2="23" y2="16"></line></svg>
-      <!-- <span>Controls</span> -->
-    </button>
-    <button class="controls__toggler-button controls--closed js-controls-toggler js-stop-propagation">
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
-      <!-- <span>Close</span> -->
-    </button>
+        <div id="controls__panel" class="js-controls-toggler js-stop-propagation">
+            <ul>
+                <li>
+                    <svg width="22" height="32" viewBox="0 0 22 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <rect x="0.75" y="0.75" width="20.5" height="30.5" rx="10.25" stroke="currentColor" stroke-width="1.5" />
+                        <path d="M10 15H5.00691C4.45463 15 4.00691 14.5523 4.00691 14V13.0333C4.00691 13.0111 4.00634 12.992 4.00529 12.9698C3.9823 12.4828 3.80705 6.23656 9.81646 4.30559C10.4173 4.11251 11 4.58892 11 5.22004V14C11 14.5523 10.5523 15 10 15Z" fill="currentColor" />
+                    </svg>
 
-    <div id="controls__panel" class="js-controls-toggler js-stop-propagation">
-      <ul>
-        <li>
-          <svg width="22" height="32" viewBox="0 0 22 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <rect x="0.75" y="0.75" width="20.5" height="30.5" rx="10.25" stroke="currentColor" stroke-width="1.5"/>
-            <path d="M10 15H5.00691C4.45463 15 4.00691 14.5523 4.00691 14V13.0333C4.00691 13.0111 4.00634 12.992 4.00529 12.9698C3.9823 12.4828 3.80705 6.23656 9.81646 4.30559C10.4173 4.11251 11 4.58892 11 5.22004V14C11 14.5523 10.5523 15 10 15Z" fill="currentColor"/>
-          </svg>
+                    <p>
+                        <strong>Tap / Left click</strong>
+                        anywhere to make the balls jump
+                    </p>
+                </li>
+            </ul>
 
-          <p>
-            <strong>Tap / Left click</strong>
-            anywhere to make the balls jump
-          </p>
-        </li>
-      </ul>
+            <p><a href="#" class="controls__panel__link">Close</a></p>
+        </div>
 
-      <p><a href="#" class="controls__panel__link">Close</a></p>
-    </div>
-
-    <script src="<?php echo $js_path; ?>game.bundle.js"></script>
-    <script src="./index.js"></script>
-  </body>
-
+        <script type="module" src="./index.js"></script>
+    </body>
 </html>
